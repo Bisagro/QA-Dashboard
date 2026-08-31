f6ecf3340164439304100feb822ecc1638b0462e
Bisagro <pmaxagt4832@shopeemobile-external.com>
2026-08-31 13:38:58 +0800

    fetch me some data

--- server.js
+++ server.js
@@ -10,16 +10,63 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("table").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const data = await client.db("table").collection("csat-table").find({}).toArray();
+    
+    buildHead();
+    buildBody(data);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+
+function buildHead() {
+  document.head.textContent = "";
+  
+  const metaCharset = createElement({ element: "meta" });
+  metaCharset.setAttribute("charset", "UTF-8");
+  
+  const title = createElement({ element: "title", textContent: "KB Manager" });
+
+  const cssLink = createElement({ element: "link", href: "root.css" });
+  cssLink.setAttribute("rel", "stylesheet");
+  
+  const favicon = createElement({ element: "link", href: "public/logo/logo.ico", type: "image/x-icon" });
+  favicon.setAttribute("rel", "icon");
+
+  const script = createElement({ element: "script", srf: "server.js" });
+
+  document.head.append(metaCharset, title, cssLink, favicon, script);
+}
+
+function buildBody(data){
+  document.body.textContent = "";
+}
+
+function createElement({element = "div", className = "", id = "", textContent = "", value = "", onClick = null, onChange = null, option = "", type = "", forWhat = "", href = "", trg = "", srf = ""}){
+  const ele = document.createElement(element);
+  if(className) ele.className = className;
+  if(id) ele.id = id;
+  if(textContent) ele.textContent = textContent;
+  if(value) ele.value = value;
+  if(onClick) ele.setAttribute("onclick", onClick);
+  if(onChange) ele.setAttribute("onchange", onClick);
+  if(option === "1") ele.readOnly = true;
+  if(option === "2") ele.disabled = true;
+  if(option === "3") ele.hidden = true;
+  if(type) ele.type = type;
+  if(forWhat) ele.setAttribute("for", forWhat);
+  if(href) ele.setAttribute("href", href);
+  if(srf) ele.setAttribute("src", srf);
+  if(trg) ele.setAttribute("target", trg);
+  
+  return ele;
+}
+
 run().catch(console.dir);
